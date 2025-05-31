f94bd228b85bc98a59f0906641c9c3013f604a38
Briandy23 <21520694@gm.uit.edu.vn>
2025-05-31 07:00:01 +0700

    extract metric

--- KGAT-pytorch/utils/metrics.py
+++ KGAT-pytorch/utils/metrics.py
@@ -160,6 +160,7 @@ def calc_metrics_at_k(cf_scores, train_user_dict, test_user_dict, user_ids, item
         
         # Negative samples: items not in the test items and not in the train items
         possible_negatives = [item for item in item_ids if item not in train_items and item not in test_item]
+        print(len(possible_negatives))
         negative_samples = np.random.choice(possible_negatives, num_negatives, replace=False)
         
         # Selected items for testing: ground truth + negative samples
